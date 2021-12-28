476c52e1d8e49d30b8067922bbe0f61b882254b8
Sorawit1234 <c335666@gmail.com>
2021-12-28 23:36:20 +0700

    init

---
+++ src/views/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Profile msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Profile from "../components/Profile.vue";
+
+@Options({
+  components: {
+    Profile,
+  },
+})
+export default class Home extends Vue {}
+</script>
